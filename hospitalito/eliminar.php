5f28cb30e95a80dd08a87a96580fe6ee3cef6a5f
RUY FELIPE PADILLA MARTINEZ <a20100349@live.ceti.mx>
2022-11-27 16:15:29 -0600

    Se añadió confirmación antes de eliminar un dato

--- hospitalito/eliminar.php
+++ hospitalito/eliminar.php
@@ -2,13 +2,53 @@
 
 include("conexionbd.php");
 
+session_start();
 
 if(isset($_GET['id']) && isset($_GET['tabla'])){
-    $tabla=$_GET['tabla'];
-    $id = $_GET['id'];
-    $sql = "DELETE FROM $tabla WHERE id_$tabla = $id";
+    $_SESSION['id'] = $_GET['id'];
+    $_SESSION['tabla'] = $_GET['tabla'];
+} else if(isset($_GET['op'])) {
+    $tabla = $_SESSION['tabla'];
+    $id    = $_SESSION['id'];
+
+    if($_GET['op'] != 'si' or !isset($_SESSION['tabla'])
+        or !isset($_SESSION['id'])) {
+        header("Location: $tabla.php");
+    }
+    $sql   = "DELETE FROM $tabla WHERE id_$tabla = $id";
     echo $sql;
-    $result = mysqli_query($conexion, $sql) or die("Error en el eliminado de datos");
+
+    try {
+        $result = mysqli_query($conexion, $sql);
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
 
     header("Location: $tabla.php");
+} else {
+    header("Location: tablas.php");
 }
+?>
+
+<?php include('includes/header.php'); ?>
+
+<main class="container p-4">
+    <h2>¿Está seguro de eliminar el dato?</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+         method="get">
+        <div>
+            <input type="radio" id="no" name="op" value="no"
+             checked>
+            <label for="no">No</label>
+        </div>
+
+        <div>
+            <input type="radio" id="no" name="op" value="no">
+            <label for="no">Si</label>
+        </div>
+
+            <input type="submit" value="Enviar">
+        </div>
+</main>
+
+<?php include('includes/footer.php'); ?>
