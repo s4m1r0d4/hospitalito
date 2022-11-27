8fce4d0dd18b8484f6a5c3d73992d1b323d65f64
danielonoide <118952764+danielonoide@users.noreply.github.com>
2022-11-27 13:44:45 -0600

    Delete hospitalito directory

--- hospitalito/conexionbd.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-
-   $conexion=mysqli_connect("localhost","root","","hospitalito") or die ("No se ha podido conectar  al servidor de la base de datos");
-
-   $db=mysqli_select_db($conexion, "hospitalito") or die("No se ha podido conectar con la base de datos");
-
-?>
